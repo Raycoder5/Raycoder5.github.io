31d124a79becdf18c1ca9ee4539be6c056e3d9bd
Raycoder5 <143038897+Raycoder5@users.noreply.github.com>
2023-10-28 18:15:09 -0400

    Started the first javaScripts html assignment

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,2 +1,14 @@
-scriptTest();
-alert("Hey my script is running");
+// Gets the current date and time of the user's computer.
+function getTheDate() {
+    const date = new Date();
+    console.log(date);
+    document.getElementById("returnDate").textContent = "Today is " + date;
+}
+
+// Greets the user by their name and how they are feeling.
+function greeting() {
+    var userName = document.getElementById("name").value;
+    var userFeeling = document.getElementById("how-you-are-doing").value;
+    document.getElementById("returnGreeting").innerHTML = "The Thompson Design Enterprises welcomes you, " + 
+    userName + "! We're glad you are doing " + userFeeling + "!";
+}
