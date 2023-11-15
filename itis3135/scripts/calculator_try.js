a64b215e57161e8b098c3752062fc8647ec4f207
Raycoder5 <143038897+Raycoder5@users.noreply.github.com>
2023-11-15 18:31:07 -0500

    My attempt at making a calculator

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,8 @@
+// I got stuck here trying to get the value of the button the user just pressed on the calculator.
+function numberInput() {
+    var userInput = document.getElementById("").value;
+}
+
+function alertNumber() {
+    alert("This button is working!")
+}
