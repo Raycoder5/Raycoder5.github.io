984369a07ad629687b134f5164401f57d98e8e33
Raycoder5 <143038897+Raycoder5@users.noreply.github.com>
2023-11-18 16:19:23 -0500

    Finished the midterm

---
+++ itis3135/hobby/scripts/hobby.js
@@ -0,0 +1,158 @@
+// Function that shows the aboutMe page.
+function aboutMe() {
+    document.getElementById("content").innerHTML = `
+    <h2>Me & my Onewheel</h2>
+    <figure>
+        <img src="images/Emma_Mini.jpg" alt="Ramone's red and black Onewheel pint called Emma mini.">
+        <figcaption><em>My Onewheel pint named "Emma mini"</em></figcaption>
+    </figure>
+    <p>
+        I first got my Onewheel on January/24th/2023 as a car replacement. I had noticed that I would go
+        short distances very often in my car that wasted a lot of gas. The Onewheel pint that I got 
+        perfectly covered my needs for those short trips to Walmart or the bus stop outside my 
+        neighborhood. 
+    </p>
+    <figure>
+        <img src="images/Emma.jpg" alt="Ramone's red Ford fusion named "Emma">
+        <figcaption><em>My red Ford fusion named "Emma"</em></figcaption>
+    </figure>
+    <p>
+        I had a car at the time I got my Onewheel and I named my car Emma. Seeing as this Onewheel pint 
+        was supposed to be a short distance car replacement, I named it Emma mini. My car was also the 
+        same shade of red as my Onewheel so it was perfect.
+    </p>
+    `;
+}
+
+// Function that shows the home page.
+function home() {
+    document.getElementById("content").innerHTML = `
+    <h2>What is a Onewheel?</h2>
+    <figure>
+        <img src="images/Onewheel_GT.jpg" alt="A person riding a Onewheel GT on the sand.">
+        <figcaption><em>A person riding a Onewheel GT on the sand.</em> Credit: Onewheel.com</figcaption>
+    </figure>
+    <p><a href="https://onewheel.com/">Onewheel</a> is an U.S based company that sells 
+        personal electric vehicles (PEVs).
+        The Onewheel is an electric skateboard type vehicle that uses a rubber go-kart tire 
+        instead of skateboard wheels. There is only one wheel on the board and that is where 
+        it gets its name from. The tire is air filled like a normal car tire so this allows 
+        riders to go through a lot more variations of terrain in comparison to a normal electric 
+        skateboard. As of 2023, Onewheel has 4 models of Onewheels that they sell: the Onewheel 
+        Pint, the Onewheel Pint x, the Onewheel GT, and the Onewheel GT-S.
+    </p>
+    <p>
+        The difference between the models is range and speed. The Onewheel pint can go 16 mph with 
+        a range of 6-8 miles and the Onewheel GT can go 20 mph with a range of 20-32 miles. 
+        There is also the Onewheel GT-S model that can go 25 mph but has a lower range of 16-25 miles. All 4
+        models of the board are assembled in California, USA. The founder and CEO Kyle Doerksen
+        intentionally kept production of the Onewheel in the U.S because he believes in giving
+        the people here at home a stable job instead of exporting the labor to China. With this
+        in mind, the "Made in USA" stamp of approval does come with an increased price tag.
+    </p>
+    <p>
+        Here are the price tags of each model as of 2023:
+    </p>
+    <p>
+        <a href="https://onewheel.com/products/pint">Onewheel Pint: $1,050 USD</a>
+    </p>
+    <p>
+        <a href="https://onewheel.com/products/onewheel-pint-x">Onewheel Pint x: $1,500 USD</a>
+    </p>
+    <p>
+        <a href="https://onewheel.com/products/onewheel-gt">Onewheel GT: $2,300 USD</a>
+    </p>
+    <p>
+        <a href="https://onewheel.com/pages/onewheel-gt-s-series">Onewheel GT-S: $3,200 USD</a>
+    </p>
+    `;
+}
+
+// Function that shows the whenStartedRiding page.
+function whenStartedRiding() {
+    document.getElementById("content").innerHTML = `
+    <h2>When I Started Riding</h2>
+    <figure>
+        <img src="images/Stock_Emma.jpg" alt="Ramone's Onewheel pint straight out the box.">
+        <figcaption><em>My Onewheel pint straight out the box.</em></figcaption>
+    </figure>
+    <p>
+        My Onewheel journey technically began in 2021 when I got my first skateboard. I just randomly
+        got one off Amazon.com and decided to teach myself and it worked. From there, I looked 
+        into getting an electric skateboard and that is how I learned about Onewheels. I saw them as 
+        superior to skateboards because they could go over dirt, grass, ice, and gravel without 
+        stopping. I mostly ride on roads but if I wanted to go off road it would be nice to not have 
+        to get off my board.
+    </p>
+    <p>
+        In the image above, you can see what a stock Onewheel pint looks like fresh out the box.
+        I customized my board so it would fit my vibe a little better. I also added a fender so if I am 
+        riding in the rain, the wheel won't throw a bunch of water and dirt up onto my shoes and pants.
+    </p>
+    `;
+}
+
+// Function that shows the whereIveRidden page.
+function whereIveRidden() {
+    document.getElementById("content").innerHTML = `
+    <h2>Where I've Ridden</h2>
+    <figure>
+        <img src="images/Emma_Sidewalk.jpg" alt="Ramone's Onewheel pint on a sidewalk.">
+        <figcaption><em>My Onewheel pint on a sidewalk near some stores.</em></figcaption>
+    </figure>
+    <p>
+        I primarily ride my Onewheel on sidewalks, roads, and grass. I've never gone offroading with 
+        my board because i'm scared I might puncture the tire or fall and hurt myself. The Onewheel pint 
+        can go offroading but the bigger models like the Onewheel GT and Onewheel GT-S are better suited 
+        for that. The aforementioned Onewheels also have an option to get a treaded tire for more grip 
+        offroad.
+    </p>
+    <p>
+        As seen in the picture above, my Onewheel has a slick racing tire so I have much better grip on 
+        roads as compared to dirt and gravel. I am currently thinking about upgrading and getting the 
+        Onewheel GT because I want the extra range and speed. The Onewheel GT is $2,300 USD BEFORE taxes 
+        so it might be a while before I can get my hands on one.
+    </p>
+    `;
+}
+
+// Function that shows the howToRide page.
+function howToRide() {
+    document.getElementById("content").innerHTML = `
+    <h2>How To Ride a Onewheel</h2>
+    <figure>
+    <a href="https://www.youtube.com/watch?v=mKYq4oeQXnw"><img src="images/How_to_Ride.jpg" alt="Onewheel Tutorial"></a>
+        <figcaption><em>A how-to video made by the Onewheel YouTube channel</em></figcaption>
+    </figure>
+    <p>
+        Click on the above image to watch the tutorial Onewheel made themselves on how to ride their 
+        board.
+    </p>
+    <p>
+        The best way to learn is to hear it straight from the makers of the board. Make sure to respect 
+        the push back and wear your helmet always!
+    </p>
+    `;
+}
+
+// Function that shows the whyYouShould page.
+function whyYouShould() {
+    document.getElementById("content").innerHTML = `
+    <h2>Why You Should Start Riding a Onewheel</h2>
+    <figure>
+        <img src="images/Pink_Pint.jpg" alt="Onewheel Pint on a pink background">
+        <figcaption><em>Onewheel Pint on a pink background</em> Credit: Onewheel.com</figcaption>
+    </figure>
+    <ul>
+        <li><p>Riding the board is so fun! IT FEELS LIKE YOU ARE FLOATING!</p></li>
+        <li><p>It can replace your short rides in your car and save gas.</p></li>
+        <li><p>It is a green and convenient way to get around.</p></li>
+        <li><p>Its made in America unlike most other personal electric vehicles (PEVs) so it 
+        supports local communities.
+        </p></li>
+    </ul>
+    <p>
+        The pricetag can be intimidating for some but the performance matches the price!
+    </p>
+    `;
+}
